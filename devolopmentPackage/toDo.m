6751d5696ce6b7c7408773f7e306f78db05bde8a
Keiland <officialkwade@gmail.com>
2018-02-13 16:27:09 -0500

    eh

--- devolopmentPackage/toDo.m
+++ devolopmentPackage/toDo.m
@@ -1,6 +1,14 @@
-  % to do, look for variablility across recording
+ 
+%1/20
+% to do, look for variablility across recording
 % slide the mean theta wave across the chuncks? 
 % calculate corrolaton for each?
 
 % still need to make the corolation plot yeah?
 
+%2/8
+% try different animals! 
+%    something is wrong... why is rio's slopes positive
+% add eeg to plotss
+% figure out how robost regression works
+% 

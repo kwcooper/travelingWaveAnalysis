f4b5ce6e8821209da55418efec4240c7f8e78b6e
Keiland <officialkwade@gmail.com>
2018-04-11 19:53:34 -0400

    added session info

---
+++ devolopmentPackage/sessions.m
@@ -0,0 +1,3 @@
+% Rat          Session                     Recording                notes        Channels                                   reference ends
+
+'RioNovelty',  '2017-07-27_16-00',          '2017-08-09_16-57-57',   'all',       [43 44 46 45 40 39 37 38 59 60 58 57 52 51 53 54], 8, []; ...

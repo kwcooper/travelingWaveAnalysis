73dd11bcc85b9dd70eaef79bd37f27f5dbc41655
Keiland <officialkwade@gmail.com>
2018-05-04 23:14:54 -0400

    temp test to make figs

--- devolopmentPackage/testr.m
+++ devolopmentPackage/testr.m
@@ -10,4 +10,64 @@ sessTst = {...
 data = [1,2,3,4,6,5,6,7,6,5,4,5,6,7,8,7,5,6,5,4,5,6,5,6,5,6,5,6,7,7,7,7,8,7,8];
 data2 = sin(data);
 
-novVsFam.
+a = {data, sessTst{2, 2}};
+a = [a; {data2, sessTst{3, 2}}];
+
+a = fliplr(a);
+
+b = {};
+for i = 1:size(sessTst, 1)
+  b = [b; {sessTst{i,3}, i * data}];
+end
+
+b = [b; {data, data}];
+b
+
+% code to compute slope figure from ratComp
+hold on;
+slopes = [];
+figure;
+for i = 1:size(ratsComp.data, 1)
+  plot(ratsComp.data{i,3}.x,ratsComp.data{i,3}.dataShift,'o');
+  hold on;
+  plot(ratsComp.data{i,3}.x,ratsComp.data{i,3}.y);
+  slopes(i,:) = ratsComp.data{i,3}.B(1);
+end
+
+% code to compute novel vs familair slopes 
+mNOV = [];
+mFAM = [];
+figure;
+for i = 1:size(ratsComp.data, 1)
+  %plot(ratsComp.data{i,3}.x,ratsComp.data{i,3}.dataShift,'o');
+  hold on;
+  if i < 6
+    c = 'r';
+    mNOV(i,:) = ratsComp.data{i,3}.B(1);
+  else
+    c = 'b';
+    mFAM(i-5,:) = ratsComp.data{i,3}.B(1);
+  end
+  
+  plot(ratsComp.data{i,3}.x,ratsComp.data{i,3}.y, c);
+end
+title('rat theta shift: NOVEL (red) vs FAM (blue)')
+
+% plot individual slope vals
+figure; hold on;
+plot(mNOV', 'b')
+plot(mFAM', 'r')
+title('novel (blue) vs fam (red) slope vals')
+
+% plot bar chart of the slope values
+figure;
+hb = bar([abs(mean(mNOV)),abs(mean(mFAM))]);
+title('average theta slope vals')
+set(gca,'xticklabel',{'novel','familiar'})
+hb(1).FaceColor = 'b';
+hb(1).FaceColor = 'r';
+
+
+
+
+

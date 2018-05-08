92e0ae601bded2c849ce58e139e5b2a8d2b1e4ec
Keiland <officialkwade@gmail.com>
2018-05-07 22:21:46 -0400

    init

---
+++ devolopmentPackage/analysis/twComputeAsym.m
@@ -0,0 +1,14 @@
+% tw grab asymetry
+function [asmScores] = twComputeAsym(root)
+
+asmScores = [];
+for i = 1:size(root.b_lfp,2)
+  root.active_lfp = i;
+  [~,~,~,asymScores] = thetaAsymmetryAnalysis(root);
+  asmScores(1:size(asymScores,1), end+1) = asymScores;
+end
+
+mean(asmScores)
+
+end
+

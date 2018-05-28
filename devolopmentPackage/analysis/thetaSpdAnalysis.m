33fdcf2fb0555f8e32cb5a5418c7ae0f361133e9
Keiland <officialkwade@gmail.com>
2018-05-28 17:12:20 -0400

    ud

--- devolopmentPackage/analysis/thetaSpdAnalysis.m
+++ devolopmentPackage/analysis/thetaSpdAnalysis.m
@@ -3,18 +3,44 @@
 function [thetaSpdMod_R] = thetaSpdAnalysis(root, chans)
 % which of the channels should we analyse? Why not all of them! muh hahahah
 
-for i = 1:chans
-root.active_lfp = i;
-[thetaSpdMod_amp, thetaSpdMod_frq, thetaSpdMod_R] = thetaSpdMod(root,[6 10],0);
-end
+metaData = root.user_def.metaData;
+plt = 1;
+
+% for i = 1:chans
+% root.active_lfp = i;
+% [thetaSpdMod_amp, thetaSpdMod_frq, thetaSpdMod_R] = thetaSpdMod(root,[6 10],0);
+% end
 
 root.active_lfp = 1;
 [thetaSpdMod_amp, thetaSpdMod_frq, thetaSpdMod_R] = thetaSpdMod(root,[6 10],0);
 
 t = thetaSpdMod_R;
-figure; imagesc(t.vel,t.frq,t.thFrq_byVelHist); title('theta Freq by velocity');
-figure; plot(t.vel,t.thAmp_byVel);
-figure; plot(t.vel,t.thFrq_byVel);
 
+
+%%
+if plt
+  figure; imagesc(t.vel,t.frq,t.thFrq_byVelHist); title(['Freq by velocity: ', metaData.Rat]);
+  
+  plotName = [metaData.Recording '_' metaData.Rat];
+  if ~exist(fullfile(metaData.savePath, 'thetaSpdHist')); mkdir(fullfile(metaData.savePath, 'thetaSpdHist')); end
+  printFigure(gcf, [fullfile(metaData.savePath, 'thetaSpdHist',[plotName,'.',metaData.fig_type])],'imgType',metaData.fig_type);
+  fprintf('Saved figure (thetaSpdHist)\n');
+  
+  
+  figure; plot(t.vel,t.thAmp_byVel); title(['theta Amp by velocity: ', metaData.Rat]);
+  
+  plotName = [metaData.Recording '_' metaData.Rat];
+  if ~exist(fullfile(metaData.savePath, 'thetaAmp')); mkdir(fullfile(metaData.savePath, 'thetaAmp')); end
+  printFigure(gcf, [fullfile(metaData.savePath, 'thetaAmp',[plotName,'.',metaData.fig_type])],'imgType',metaData.fig_type);
+  fprintf('Saved figure (thetaAmp)\n');
+  
+  
+  figure; plot(t.vel,t.thFrq_byVel); title(['theta Freq by velocity: ', metaData.Rat]);
+  
+  plotName = [metaData.Recording '_' metaData.Rat];
+  if ~exist(fullfile(metaData.savePath, 'thetaSpd')); mkdir(fullfile(metaData.savePath, 'thetaSpd')); end
+  printFigure(gcf, [fullfile(metaData.savePath, 'thetaSpd',[plotName,'.',metaData.fig_type])],'imgType',metaData.fig_type);
+  fprintf('Saved figure (thetaSpd)\n');
+end
 end
 
